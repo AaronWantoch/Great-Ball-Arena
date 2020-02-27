1deefd576ab2b11c9b1128d1323d1815fa28f1cd
credus <aaron.wantoch@gmail.com>
2020-02-27 19:19:30 +0100

    Initial Commit

---
+++ Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    [SerializeField] Vector3 offset;
+
+    [SerializeField] Transform player;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.localPosition = player.localPosition + offset;
+        transform.LookAt(player);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+
+    Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speedPerFrame = speed * Time.deltaTime;
+        rigidbody.AddForce(
+            CrossPlatformInputManager.GetAxis("Horizontal") * speedPerFrame,
+            0,
+            CrossPlatformInputManager.GetAxis("Vertical") * speedPerFrame);
+    }
+}
